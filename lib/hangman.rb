91b23d91e159d3c39ac7ace07b7f8b26cca53797
Denis Lisunov <rev.denisas@gmail.com>
2021-09-09 21:53:31 +0300

    Game logic established

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,17 +4,15 @@ class Game
     @word = "hello".downcase
     @turns = '|' * 12
     @letters_used = []
+    @letter_count = '_' * @word.length
     play_game()
   end
 
+  private
+
   def play_game()
-    letter_count = '_' * @word.length
-    while letter_count.index('_') != nil && @turns.length > 0
-    system('clear')
-    puts letter_count
-    puts "letters used: #{@letters_used} "
-    puts "Turns #{@turns}"
-    print "Enter a guess: "
+    while @letter_count.index('_') != nil && @turns.length > 0
+    print_info()
     guess = gets.chomp.downcase
     until @letters_used.index(guess) == nil && guess.length == 1 && guess.match?(/[a-z]/)
       print "Invalid input. Try again: "
@@ -24,13 +22,33 @@ class Game
     if @word.index(guess) != nil
       @word.split('').each_with_index do |char, index|
         if char == guess
-	  letter_count[index] = guess
+	  @letter_count[index] = guess
 	end
       end
     else
       @turns = @turns.chop
     end
    end
+   ending()
+  end
+
+  def print_info()
+    
+    system('clear')
+    puts @letter_count
+    puts "letters used: #{@letters_used.join(" ")} "
+    puts "Turns #{@turns}"
+    print "Enter a guess: "
+
+  end
+
+  def ending()
+
+    if @letter_count.index('_') == nil
+      puts "You Win!"
+    else
+      puts "You Loose! The answer was #{@word}!"
+    end
   end
 
 
